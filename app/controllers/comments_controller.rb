23f6011d1749ee40334e4888a7b6506831d10116
forrest austin <faustin0494@gmail.com>
2016-04-28 08:13:01 -0400

    created views

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,45 @@
 class CommentsController < ApplicationController
+
+	before_action :set_comment, only: [:edit, :update, :destroy]
   def edit
   end
+
+  
+    def create
+  	@comment = Comment.new(comment_params)
+
+  	respond_to do |format|
+  		if @comment.save
+  			format.html {redirect_to blog_post_path(id: @comment.blog_post_id), notice: "Comment was created successfully."}
+  		else
+  			format.html {redirect_to blog_post_path(id: @comment.blog_post_id), notice: "comment failed to post"}
+  		end
+  	end
+  end
+
+  def update
+	respond_to do |format|
+		if @comment.update(comment_params)
+			format.html {redirect_to blog_post_path(id: @comment.blog_post_id), notice: "comment was updated successfully"}
+		else
+			format.html {render :edit}
+  end
+end
+end
+
+def destroy
+	@comment.destroy
+	respond_to do |format|
+		format.html {redirect_to blog_post_path(id: @comment.blog_post_id), notice: "Your comment was destroyed"}
+end
+end
+
+private
+	def comment_params
+		params.require(:comment).permit(:author, :comment_entry, :blog_post_id)
+	end
+
+	def set_comment
+		@Comment = Comment.find(params[:id])
+	end
 end
